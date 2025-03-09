3e57d30c6eec72d878d04978658d2faabed628f2
DeleonZander <deleonzandereindrex@gmail.com>
2025-03-09 20:32:53 +0800

    Add files via upload

---
+++ ipo5.py
@@ -0,0 +1,11 @@
+age = int(input("What is your age?:"))
+if age >=65:
+    print("You are a senior")
+elif age >=20:
+    print("You are an adult")
+elif age >=13:
+    print("You are a teen")
+elif age >=0:
+    print("You are a child")
+else:
+    print("Invalid Age")
